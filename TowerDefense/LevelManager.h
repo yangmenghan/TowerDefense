68a807f752349699f666e20ad49f2ea04a2180ba
Eorielle <yangmenghan@gmail.com>
2014-10-16 16:37:25 +0200

    LevelManager & Enemy modification
    LevelManager : add getLevelManager/removeEnemy(int)/ removeTower(int)
    methods;
    
    Enemy : add getDistanceToTarget Method

--- TowerDefense/LevelManager.h
+++ TowerDefense/LevelManager.h
@@ -2,23 +2,32 @@
 #include "Enemy.h"
 #include "Tower.h"
 #include <vector>
+#include <memory>
 #include <SFML\Graphics.hpp>
 
 using namespace std;
 
 class LevelManager{
 public:
-	LevelManager getLevelManager();
+	
 	void kill();
 
 	void gameLoop();
 	void addEnemy(Enemy);
 	void removeEnemy(Enemy);
+	void removeEnemy(int);
 	void addTower(Tower);
 	void removeTower(Tower);
+	void removeTower(int);
 
-	static LevelManager levelManager = NULL;
+	vector<Enemy>* getEnemies();
+	vector<Tower>* getTowers(); 
+	LevelManager* getLevelManager();
+
+	static LevelManager* levelManager;
 private:
+	LevelManager();
+	~LevelManager();
 	vector<Enemy> enemies;
 	vector<Tower> towers;
 };
