e12dbe631db218e56c2116121cd96ab15d6ffabb
Higanbana <france.fs.wang@gmail.com>
2014-10-16 14:16:44 +0200

    Small changes
    to a couple of classes

---
+++ TowerDefense/CreditsMenu.cpp
@@ -0,0 +1,24 @@
+#include "CreditsMenu.h"
+
+string getCreditsFile()
+{
+	return CREDITS_ADD;
+}
+
+sf::Font getFont()
+{
+	return font;
+}
+
+void setFont()
+{
+
+}
+
+
+
+void rollCredits()
+{
+	sf::Text credits;
+
+}
