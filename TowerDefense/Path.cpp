142b95964b7437d1cb7c385698af01063f89bf7d
ViolaineJU <violaineju@gmail.com>
2014-10-24 13:07:43 +0200

    changes of Path

--- TowerDefense/Path.cpp
+++ TowerDefense/Path.cpp
@@ -1,7 +1,30 @@
 #include "Tile.h";
 #include "Path.h";
 #include <SFML\Graphics.hpp>
+#include <vector>
 
 using namespace std;
 
-void draw();
+Path::Path(vector<Tile> a)
+{
+	path = a;
+};
+Path::Path()
+{
+	vector<Tile> b;
+	path = b;
+};
+
+
+void Path::draw(vector<Tile> path)
+{
+	for (int i = 0; i < path.size(); i++)
+	{
+		Tile tile = path[i];
+
+	};
+
+
+};
+	
+
