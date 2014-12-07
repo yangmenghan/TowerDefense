b3faa94ef6e5e64ed07e719996ffc30d246868b0
ViolaineJU <violaineju@gmail.com>
2014-12-07 21:05:29 +0100

    ...

--- TowerDefense/Path.cpp
+++ TowerDefense/Path.cpp
@@ -17,7 +17,7 @@ Path::Path()
 
 void Path::draw(sf::RenderWindow &w)
 {
-	sf::CircleShape shape(10); 
+	sf::CircleShape shape(4);  
 	shape.setFillColor(sf::Color(100, 250, 50));
 	
 	for (int i = 0; i < path.size(); i++)
@@ -26,8 +26,8 @@ void Path::draw(sf::RenderWindow &w)
 		shared_ptr<Tile> tile = path[i];
 		sf::Vector2i vec = tile->getPositionPixel();
 		sf::Vector2f vec1;
-		vec1.x =vec.x + 25;
-		vec1.y =vec.y + 25;
+		vec1.x =vec.x + 21;
+		vec1.y =vec.y + 21;
 		shape.setPosition(vec1);
 		w.draw(shape);
 
@@ -38,7 +38,6 @@ void Path::draw(sf::RenderWindow &w)
 
 vector<shared_ptr<Tile>> Path::getPath()
 {
-	Path::draw();
 	return path;
 };
 	
