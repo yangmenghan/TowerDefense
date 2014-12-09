a07f0bf1a02203050242c8bd6d5c5c82d6412f24
Higanbana <france.fs.wang@gmail.com>
2014-12-09 08:19:53 +0100

    Correction of display

--- TowerDefense/GameMenu.cpp
+++ TowerDefense/GameMenu.cpp
@@ -120,13 +120,13 @@ void GameMenu::draw(sf::RenderWindow& w)
 	
 	//set position and font for the text displays
 
-	std::string lifeCount = std::to_string(levelManager->getPlayer().getHP());
+	std::string lifeCount = std::to_string((levelManager->getPlayer())->getHP());
 	lifeCountDisplay.setString("Player life : " + lifeCount+"/10");
 
-	std::string pointsCount = std::to_string(levelManager->getPlayer().getScore());
+	std::string pointsCount = std::to_string((levelManager->getPlayer())->getScore());
 	pointsCountDisplay.setString("Player score : " + pointsCount);
 
-	string waveCount = std::to_string(levelManager->getCurrentWaveNumber());
+	string waveCount = std::to_string((levelManager->getCurrentWaveNumber()));
 	waveCountDisplay.setString("Wave " + waveCount + "/24");
 
 	w.draw(lifeCountDisplay);
