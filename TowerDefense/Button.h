279f666c6c636ed9b053c63fac96291b0af326a7
Higanbana <france.fs.wang@gmail.com>
2014-12-13 19:25:53 +0100

    lots of comments

--- TowerDefense/Button.h
+++ TowerDefense/Button.h
@@ -22,28 +22,93 @@ private:
 	bool isClicked;
 
 public:
-//Constructors and destroyers
+//! Default constructor
 	Button();
+
+//! Constructor
+/*!
+	\param myTextureAddress the file address of the texture as a string
+	\param mySize an SFML 2D Vector of integers that will define the size of the button
+	\param myPosition an SFML 2D Vector of integers that will set the position of the button (origin = top left corner)
+	\param n an integer which describes the number of sprites in the texture
+*/
 	Button(const std::string myTextureAddress, sf::Vector2i mySize, sf::Vector2i myPosition, int n);
+
+//! Destructor
 	~Button();
 
-//Getters
+//! Position getter
+/*!
+	\return position of the button
+*/
 	sf::Vector2i getPosition();
+
+//! Size getter
+/*!
+	\return size of the button
+*/
 	sf::Vector2i getSize();
-	sf::Sprite getSprite();
+
+//! Sprite getter
+/*!
+	\return the current sprite number
+*/
+	int getSpriteNumber();
+
+//! Check click
+/*!
+	checks if button was clicked
+	\return true if the button was clicked
+*/
 	bool checkClick();
+
+//! Check click
+/*!
+	checks if button is hovered by the moused
+	\return true if the button is being hovered
+*/
 	bool checkHover();
 
-//Setters
+//! Set position
+/*!
+	sets the position of the button to a new one
+	\param mPosition the target position as an SFML 2D vector of integers
+*/
 	void setPosition(sf::Vector2i mPosition);
+
+//! Set size
+/*!
+	changes the size of the button
+	\param mSize the new size of the button
+*/
 	void setSize(sf::Vector2i mSize);
-	void setSprite(sf::Sprite mSprite);
-	void setClickedState(bool mIsClicked);
 
-//Functions
-	void draw(sf::RenderWindow&); 
+//! Display function
+/*!
+	Displays the button at the current sprite number at the right size and position
+	\param w the current RenderWindow in which the button has to be displayed
+*/
+	void draw(sf::RenderWindow& w); 
+
+//! Hovering check function
+/*!
+	Will check in a given window whether or not the button is hovered.
+	\param w the current RenderWindow in which the hovering is or is not taking place
+	\sa bool checkHover();
+*/
 	bool mouseHover(sf::RenderWindow& w);
-	void Button::resolveEvent(sf::Event event);
-	void spriteUpdate(int i);
 
+//! Resolve Event
+/*!
+	Handles all events that take place in the button and determine whether he was clicked or not
+	\param event an SFML event that will be handled
+*/
+	void Button::resolveEvent(sf::Event event);
+
+//! Sprite Update
+/*!
+	Handles the changes in the sprite display
+	\param i the wanted sprite number as an integer
+*/
+	void spriteUpdate(int i);
 };
