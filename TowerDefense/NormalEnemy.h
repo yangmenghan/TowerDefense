5e01dd255bdab0eb4d7f84f161c949fbb287e15e
Eorielle <yangmenghan@gmail.com>
2014-12-13 18:45:25 +0100

    add unit test's file and some comments

--- TowerDefense/NormalEnemy.h
+++ TowerDefense/NormalEnemy.h
@@ -4,11 +4,23 @@
 
 using namespace std;
 
+//! Class of normal enemy
+/*! Enemy with normal speed and normal HP */
 class NormalEnemy : public Enemy{
 public:
+	//! normal constructor. 
 	NormalEnemy();
-	NormalEnemy(int hp, float defence, int bounty, int scoreValue, sf::Sprite sprite, float speed);
-	virtual ~NormalEnemy(){};
 
-	string test(){ return ""; };
+	//! constructor with specific caracteristics
+	/*!
+	\param hp Enemy's maximum hp, integer
+	\param defence Enemy's defence, float value, not used yet
+	\param bounty integer money that the player get when the enemy is killed
+	\param scoreValue integer number of point that the player get when the enemy is killed
+	\param speed speed of the enemy, float value
+	*/
+	NormalEnemy(int hp, float defence, int bounty, int scoreValue, sf::Sprite sprite, float speed);
+	
+	//! normal destructor.
+	~NormalEnemy(){};
 };
--- TowerDefense/ToughEnemy.h
+++ TowerDefense/ToughEnemy.h
@@ -4,12 +4,24 @@
 
 using namespace std;
 
+//! Class of tough enemy
+/*! Enemy with slow speed and many HP */ 
 class ToughEnemy : public Enemy{
 public:
+	//! normal constructor. 
 	ToughEnemy();
-	ToughEnemy(int hp, float defence, int bounty, int scoreValue, sf::Sprite sprite, float speed);
-	virtual ~ToughEnemy(){};
 
-	string test(){ return ""; };
+	//! constructor with specific caracteristics
+	/*! 
+	\param hp Enemy's maximum hp, integer
+	\param defence Enemy's defence, float value, not used yet
+	\param bounty integer money that the player get when the enemy is killed
+	\param scoreValue integer number of point that the player get when the enemy is killed
+	\param speed speed of the enemy, float value
+	*/
+	ToughEnemy(int hp, float defence, int bounty, int scoreValue, sf::Sprite sprite, float speed);
+
+	//! normal destructor.
+	~ToughEnemy(){};
 
 };
